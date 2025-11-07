c60388074effa29a22c405304fb0b5e50b4f8a10
DemusOpalium <majordemus@yahoo.de>
2025-11-07 16:34:43 +0100

    Handle participant disconnect lifecycle (#24)

--- src/main/java/de/opalium/luckysky/listeners/PlayerListener.java
+++ src/main/java/de/opalium/luckysky/listeners/PlayerListener.java
@@ -7,6 +7,9 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 public class PlayerListener implements Listener {
@@ -38,5 +41,20 @@ public class PlayerListener implements Listener {
         } else {
             plugin.game().handleRespawn(player);
         }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        plugin.game().handleQuit(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        plugin.game().handleJoin(event.getPlayer());
+    }
+
+    @EventHandler
+    public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
+        plugin.game().handleJoin(event.getPlayer());
     }
 }
